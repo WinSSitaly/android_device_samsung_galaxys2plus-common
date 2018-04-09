512e2825b91dbe0e58be69b0c4afc3daa025134b
osas514 <peaceosas89@gmail.com>
2018-04-09 21:37:28 +0100

    I9082: Selinux: Later

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -137,9 +137,13 @@ ANDROID_NO_TEST_CHECK := true
 
 # SELinux
 <<<<<<< HEAD:BoardConfigCommon.mk
+<<<<<<< HEAD:BoardConfigCommon.mk
 BOARD_SEPOLICY_DIRS += device/samsung/galaxys2plus-common/sepolicy
 =======
 BOARD_SEPOLICY_DIRS += device/samsung/i9082/sepolicy
+=======
+#BOARD_SEPOLICY_DIRS += device/samsung/i9082/sepolicy
+>>>>>>> 2303205... I9082: Selinux: Later:BoardConfig.mk
 
 TARGET_USES_LEGACY_ADB_INTERFACE := true
 >>>>>>> de7a491... I9082: Enable legacy_adb interface:BoardConfig.mk
